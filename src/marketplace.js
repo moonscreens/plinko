f25929a8eb2c68fa77831111b3eebb5c12342e2f
calebbabin <object.caleb@gmail.com>
2022-09-05 21:43:34 -0500

    add grid to chart

--- src/marketplace.js
+++ src/marketplace.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three';
+import colors from './colors';
 import { boardWidth } from './config';
 import { glassMaterial } from './materials';
 import { scene } from './scene';
@@ -14,11 +15,21 @@ main.position.x = boardWidth - chartWidth / 3;
 scene.add(main);
 
 const backplane = new THREE.Mesh(
-    new THREE.PlaneGeometry(chartWidth, chartHeight, 1, 1),
-    glassMaterial
+	new THREE.PlaneGeometry(chartWidth, chartHeight, 1, 1),
+	glassMaterial
 );
 backplane.position.z = -0.1;
 main.add(backplane);
+
+const grid = new THREE.Mesh(
+	new THREE.PlaneGeometry(chartWidth * 0.9, chartHeight * 0.7, Math.round(chartWidth * 1.5), Math.round(chartHeight * 1.5)),
+	new THREE.MeshBasicMaterial({
+		color: colors.green.clone().multiplyScalar(0.2),
+		wireframe: true,
+	})
+);
+grid.position.z = -0.05;
+main.add(grid);
 
 const backplaneText = generateTextPlane(chartWidth, chartWidth * 0.08, 30, 'Total Value On Board');
 backplaneText.mesh.position.y = chartHeight / 2 + 0.6;
@@ -27,7 +38,7 @@ backplaneText.mesh.layers.toggle(LAYERS.bloom);
 
 
 const valueCountText = generateTextPlane(chartWidth, chartWidth * 0.05, 30, '0');
-valueCountText.mesh.position.y = chartHeight / 2 -0.3;
+valueCountText.mesh.position.y = chartHeight / 2 - 0.3;
 main.add(valueCountText.mesh);
 valueCountText.mesh.layers.toggle(LAYERS.bloom);
 
@@ -41,68 +52,68 @@ emoteCountText.mesh.layers.toggle(LAYERS.bloom);
 
 
 export const activateMarketplace = (TMIClient) => {
-    TMIClient.on('message', (channel, userstate, message, self) => {
-        /*console.log(
-            channel,
-            userstate.username,
-            message
-        );*/
-    });
+	TMIClient.on('message', (channel, userstate, message, self) => {
+		/*console.log(
+			channel,
+			userstate.username,
+			message
+		);*/
+	});
 };
 
 
 const totalValueArray = new Array(30).fill(0);
 const totalValueGeometry = new THREE.BufferGeometry();
 totalValueGeometry.setAttribute(
-    'position',
-    new THREE.BufferAttribute(
-        new Float32Array(totalValueArray.length * 3),
-        3
-    ),
+	'position',
+	new THREE.BufferAttribute(
+		new Float32Array(totalValueArray.length * 3),
+		3
+	),
 );
 
 const totalValueLine = new THREE.Line(
-    totalValueGeometry,
-    new THREE.MeshBasicMaterial({
-        color: 0x2288ff
-    })
+	totalValueGeometry,
+	new THREE.MeshBasicMaterial({
+		color: 0x2288ff
+	})
 );
 main.add(totalValueLine);
 totalValueLine.scale.set(chartWidth * 0.9, chartHeight * 0.7);
-totalValueLine.position.x -= chartWidth / 2;
+totalValueLine.position.x -= chartWidth / 2 - chartWidth * 0.05;
 
 const totalValueLineMax = 250;
 
 const recalcTotalValue = () => {
-    let temp = 0;
-    for (let index = 0; index < activeBoardEmotes.length; index++) {
-        const element = activeBoardEmotes[index];
-        temp += isNaN(element.myScore) ? 0 : element.myScore;
-        if (isNaN(element.myScore)) {
-            console.log(element.onHit);
-            element.myScore = 0;
-        }
-    };
-    totalValueArray.push(temp / totalValueLineMax);
-    totalValueArray.splice(0, 1);
+	let temp = 0;
+	for (let index = 0; index < activeBoardEmotes.length; index++) {
+		const element = activeBoardEmotes[index];
+		temp += isNaN(element.myScore) ? 0 : element.myScore;
+		if (isNaN(element.myScore)) {
+			console.log(element.onHit);
+			element.myScore = 0;
+		}
+	};
+	totalValueArray.push(temp / totalValueLineMax);
+	totalValueArray.splice(0, 1);
 
-    valueCountText.updateText(temp.toLocaleString() + ' points');
-    emoteCountText.updateText(activeBoardEmotes.length + ' emotes');
+	valueCountText.updateText(temp.toLocaleString() + ' points');
+	emoteCountText.updateText(activeBoardEmotes.length + ' emotes');
 
-    shiftGeometryLeft(totalValueGeometry);
+	shiftGeometryLeft(totalValueGeometry);
 
-    const positions = totalValueGeometry.getAttribute('position');
-    for (let index = 0; index < positions.count * positions.itemSize; index += positions.itemSize) {
-        //console.log(index / 3, positions.array[index], positions.array[index + 1], positions.array[index + 2]);
-        const valueIndex = index / 3;
-        positions.array[index] = valueIndex / (totalValueArray.length - 1);
-        positions.array[index + 1] = totalValueArray[valueIndex] - 0.5;
-    }
-    totalValueGeometry.setAttribute('position', positions);
-    totalValueGeometry.attributes.position.needsUpdate = true;
+	const positions = totalValueGeometry.getAttribute('position');
+	for (let index = 0; index < positions.count * positions.itemSize; index += positions.itemSize) {
+		//console.log(index / 3, positions.array[index], positions.array[index + 1], positions.array[index + 2]);
+		const valueIndex = index / 3;
+		positions.array[index] = valueIndex / (totalValueArray.length - 1);
+		positions.array[index + 1] = totalValueArray[valueIndex] - 0.5;
+	}
+	totalValueGeometry.setAttribute('position', positions);
+	totalValueGeometry.attributes.position.needsUpdate = true;
 }
 recalcTotalValue();
 
 setInterval(() => {
-    recalcTotalValue();
+	recalcTotalValue();
 }, 1000);
