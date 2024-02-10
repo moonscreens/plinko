fe6e0f342bc888303e1f2ab77d314d3a3914dbc5
calebbabin <object.caleb@gmail.com>
2024-02-09 19:45:28 -0600

    try fixing deployment issue

--- src/physWorld.js
+++ src/physWorld.js
@@ -1,3 +1,9 @@
 import RAPIER from "@dimforge/rapier2d";
 
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+await sleep(500);
+
 export const world = new RAPIER.World({x: 0, y: -15});
